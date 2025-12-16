0a47090b8526ed287d7b1de10ba3fa3bda8bb8c0
staytoncoryjoseph-tech <staytoncoryjoseph@gmail.com>
2025-12-15 19:14:28 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "healthhack-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./style-anim.css",
+  "./app.js",
+  "./ai-profiles.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
